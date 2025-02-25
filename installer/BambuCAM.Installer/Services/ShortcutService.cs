255995c803b9aeb18dac13e7abd7126bf66c932f
Bangertech <info@bangertech.de>
2025-02-25 12:11:02 +0100

    updated windows installer v.5

--- installer/BambuCAM.Installer/Services/ShortcutService.cs
+++ installer/BambuCAM.Installer/Services/ShortcutService.cs
@@ -1,23 +1,23 @@
 using System;
 using System.IO;
-using IWshRuntimeLibrary;
+using System.Runtime.InteropServices;
 
 namespace BambuCAM.Installer.Services
 {
     public class ShortcutService
     {
-        public void CreateDesktopShortcut(string targetPath, string shortcutName)
+        public void CreateDesktopShortcut(string targetUrl, string shortcutName)
         {
-            var shell = new WshShell();
             var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            var shortcutPath = Path.Combine(desktopPath, $"{shortcutName}.lnk");
+            var shortcutPath = Path.Combine(desktopPath, $"{shortcutName}.url");
 
-            var shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
-            shortcut.TargetPath = targetPath;
-            shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath);
-            shortcut.Description = "BambuCAM - 3D Printer Monitor";
-            shortcut.IconLocation = Path.Combine(Path.GetDirectoryName(targetPath), "Assets", "setup-icon.ico");
-            shortcut.Save();
+            using (StreamWriter writer = new StreamWriter(shortcutPath))
+            {
+                writer.WriteLine("[InternetShortcut]");
+                writer.WriteLine($"URL={targetUrl}");
+                writer.WriteLine("IconIndex=0");
+                writer.WriteLine($"IconFile={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "setup-icon.ico")}");
+            }
         }
     }
 } 
